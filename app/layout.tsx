dbccca4062f03647f5ed4d8ecb6ce1faaebb2455
DyCodeBlock <iam.zandy88@gmail.com>
2023-06-10 19:21:13 +0800

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import { Footer, Navbar } from '@/components'
+
+export const metadata = {
+  title: 'CarDagul',
+  description: 'Discover the best cars in the Philippines',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
